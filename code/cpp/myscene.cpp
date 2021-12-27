3158738110466e89005252a0fb578460874100c0
FranekStratovarius <32364106+FranekStratovarius@users.noreply.github.com>
2021-12-27 11:52:57 +0100

    character aufgeräumt und kann jetzt mehrere meshes haben

--- code/cpp/myscene.cpp
+++ code/cpp/myscene.cpp
@@ -10,17 +10,12 @@
 #include "simplecube.h"
 #include "color.h"
 
-#include "physicaccessablecamera.h"
 #include "headers/characterticker.h"
 
 Node* initScene1();
 
 void SceneManager::initScenes()
 {
-	//Camera* cam = new Camera();
-	//CameraController* camController = new MouseKeyboardCameraController(cam);
-	//Q_UNUSED(camController);
-
 	PhysicAccessableCamera* cam = new PhysicAccessableCamera;
 
 	RenderingContext* myContext = new RenderingContext(cam);
@@ -29,12 +24,10 @@ void SceneManager::initScenes()
 	ScreenRenderer* myRenderer = new ScreenRenderer(myContextNr, myScene);
 	Q_UNUSED(myRenderer);
 
-	//MouseKeyboardCameraController Test1(cam);
-
 	// Vorsicht: Die Szene muss initialisiert sein, bevor das Fenster verändert wird (Fullscreen)
 	SceneManager::instance()->setActiveScene(myScene);
 	SceneManager::instance()->setActiveContext(myContextNr);
-	//SceneManager::instance()->setFullScreen();
+	SceneManager::instance()->setFullScreen();
 }
 
 Node* initScene1()
@@ -45,33 +38,7 @@ Node* initScene1()
 	int v_Slot = PhysicEngineManager::createNewPhysicEngineSlot(PhysicEngineName::BulletPhysicsLibrary);
 	PhysicEngine* v_PhysicEngine = PhysicEngineManager::getPhysicEngineBySlot(v_Slot);
 
-
-	Drawable* PlayerObject = new Drawable(new SimpleCube(1.0, 1.0, 1.0));
-	Color* c;
-	c = PlayerObject->getProperty<Color>();
-	c->setValue(1.0, 1.0, 0.0, 1.0);
-	Node* PlayerNode = new Node(PlayerObject);
-
-	//Das Playerobjekt nach oben verschieben, damit er nicht in der Plane drinnen ist
-	ModelTransformation* modelTransform;
-	modelTransform = PlayerObject->getProperty<ModelTransformation>();
-	modelTransform->translate(0.f, 1.f, 0.f);
-	root->addChild(PlayerNode);
-
-	// Character Objekt erzeugen mit einer Verfolgerkamera
-	DynamicCharacterWithCam* CharacterWithCam = v_PhysicEngine->createNewDynamicCharacterWithCam(PlayerObject);
-	// Kamera anhängen, die bestimmung der kameraposition wird nun komplett von dem character übernommen,  allein
-	// die x achsendrehung kann noch beeinflusst werden
-	CharacterWithCam->setCam(dynamic_cast<PhysicAccessableCamera*>(SceneManager::instance()->getActiveContext()->getCamera()));
-	// Relative Kameraposition zum Drawable setzen
-	CharacterWithCam->setRelativeCamPosition(0.f, 4.f, 5.f);	//macht noch garnichts
-	CharacterWithCam->setUpDownView(-30.0F);					//macht noch garnichts
-	// Physic Object registrieren
-	PlayerObject->getPhysicObject()->registerPhysicObject();
-
-
-	// Character Ticker der für die Steuerung unser Charaktere veranwortlich ist
-	CharacterTicker* test = new CharacterTicker(CharacterWithCam);
+	CharacterTicker* spieler = new CharacterTicker(root,v_PhysicEngine);
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -87,7 +54,7 @@ Node* initScene1()
 	planePhys->setConstructionInfo(ConstructionInfo);
 	planePhys->registerPhysicObject();
 
-	c = plane->getProperty<Color>();
+	Color* c = plane->getProperty<Color>();
 	c->setValue(1.0, 0.0, 0.0, 1.0);
 
 	root->addChild(planeNode);
---
+++ code/shaders/transparent.frag
@@ -0,0 +1,5 @@
+layout (location = 0) out vec4 fragColor;
+
+void main(){
+    fragColor = vec4(0.0f,0.0f,0.0f,0.0f);
+}
