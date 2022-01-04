cba7e6f9a8e35beb024d25883c323b50390fd148
JonasDietrich2000 <75167306+JonasDietrich2000@users.noreply.github.com>
2022-01-04 12:20:02 +0100

    Tor angepasst
    Kaputte if/else Bedingung repariert

--- code/headers/tor.h
+++ code/headers/tor.h
@@ -8,15 +8,15 @@ class Tor{
 		Tor(Transformation* node, float posY_input, int form) : tor_transform(node), posY(posY_input), tor_form(form) {};
 		void update(int spieler_form){ 
             if (spieler_form == tor_form) {
-                if (posY == 0.7f) {
-                    tor_transform->translate(0, -10, 0);
-                    posY -= 10;
+                if (posY <= 0.7f && pos > -10.7f) {
+                    tor_transform->translate(0, -0.01f, 0);
+                    posY -= 0.01f;
                 }
             }
             else {
-                if (posY == -10.7f) {
-                    tor_transform->translate(0, 10, 0);
-                    posY += 10;
+                if (posY >= -10.7f && posY < 0.7f) {
+                    tor_transform->translate(0, 0.01f, 0);
+                    posY += 0.01f;
                 }
             }
 		};
